a1914af6d2f579392f83b32fd408e695eba85210
Eric Mignot <eric.mignot@gmail.com>
2018-08-01 11:52:52 -0700

    explore publish via Http POST

--- src/test/java/ericminio/activemq/HttpPublishTest.java
+++ src/test/java/ericminio/activemq/HttpPublishTest.java
@@ -2,9 +2,14 @@ package ericminio.activemq;
 
 import ericminio.support.AsyncHttpResponse;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.broker.BrokerRegistry;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.web.MessageServlet;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.Result;
+import org.eclipse.jetty.client.util.BufferingResponseListener;
+import org.eclipse.jetty.client.util.InputStreamContentProvider;
+import org.eclipse.jetty.client.util.StringContentProvider;
+import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -15,13 +20,15 @@ import org.junit.Test;
 import javax.jms.Connection;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
+import java.io.ByteArrayInputStream;
 import java.net.URI;
+import java.util.concurrent.CountDownLatch;
 
 import static ericminio.support.AsyncGetRequest.asyncGet;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class HttpReadTest {
+public class HttpPublishTest {
 
     protected BrokerService broker;
     protected Session session;
@@ -59,7 +66,6 @@ public class HttpReadTest {
         server.start();
     }
 
-
     @After
     public void stopAll() throws Exception {
         session.close();
@@ -70,9 +76,17 @@ public class HttpReadTest {
     }
 
     @Test
-    public void isDoneViaGet() throws Exception {
+    public void isDoneViaPost() throws Exception {
         AsyncHttpResponse response = asyncGet("http://localhost:8888/message/this-queue?type=queue");
-        producer.send(session.createTextMessage("hello"));
+        String content = "body=hello";
+        HttpClient httpClient = new HttpClient();
+        httpClient.start();
+        httpClient
+            .newRequest("http://localhost:8888/message/this-queue?type=queue")
+            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .content(new StringContentProvider(content))
+            .method(HttpMethod.POST)
+            .send();
 
         assertThat(response.getBody(), equalTo("hello"));
     }
