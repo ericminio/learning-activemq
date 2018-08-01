cb18914f9151e78e0e094f217a9fe365e29445d0
Eric Mignot <eric.mignot@gmail.com>
2018-08-01 12:01:05 -0700

    code format

--- src/test/java/ericminio/support/ThirdParty.java
+++ src/test/java/ericminio/support/ThirdParty.java
@@ -10,10 +10,10 @@ public class ThirdParty {
         HttpClient httpClient = new HttpClient();
         httpClient.start();
         httpClient
-                .newRequest(uri)
-                .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-                .content(new StringContentProvider(content))
-                .method(HttpMethod.POST)
-                .send();
+            .newRequest(uri)
+            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .content(new StringContentProvider(content))
+            .method(HttpMethod.POST)
+            .send();
     }
 }
